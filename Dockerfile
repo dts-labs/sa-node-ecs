0f02f8eaafc3889841069e136201646b41197e11
David <david92rl@gmail.com>
2016-05-02 00:11:48 +0200

    First commit

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM debian:jessie
+
+RUN apt-get update
+
+RUN apt-get install -y curl
+
+RUN curl -sL https://deb.nodesource.com/setup_4.x | bash -
+
+RUN apt-get install -y nodejs
+
+WORKDIR /src
+ADD . /src
+RUN . ~/.bashrc
+
+RUN ls -la
+
+RUN npm install
+
+EXPOSE 8080
+
+CMD [ "npm", "start" ]
---
+++ deploy.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+set -o pipefail
+
+JQ="jq --raw-output --exit-status"
+
+deploy_image() {
+    echo "#### Deploying image"
+    echo "# Login"
+    eval "$(aws ecr get-login --region eu-west-1)"
+    echo "# Building"
+    docker build -t awesome .
+    echo "# Tagging"
+    docker tag awesome:latest 567141585396.dkr.ecr.eu-west-1.amazonaws.com/awesome:latest
+    echo "# Pushing"
+    docker push 567141585396.dkr.ecr.eu-west-1.amazonaws.com/awesome:latest
+    echo "Image deployed!"
+}
+
+update_service() {
+    echo "Creating new task/revision"
+    aws ecs register-task-definition --cli-input-json file://task.json
+
+    echo "Updating Service"
+    aws ecs update-service --cluster $AWS_CLUSTER --service $AWS_SERVICE --task-definition awesomeTask
+
+    echo "Updated! Let AWS do the rest"
+}
+
+deploy_image
+update_service
