3723d6dc9edfcd86af7d26bccf7e0c53cb5a253d
David <david92rl@gmail.com>
2016-05-02 14:10:31 +0200

    New message

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-   res.end("Hi, you're Super Awesome!");
+   res.end("Hi, you're Super Awesome now!");
 })
 
 var server = app.listen(8080, function () {
