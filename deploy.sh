2c0b88e45516140cba302818acba4555c6631ee0
David <david92rl@gmail.com>
2016-05-02 12:51:39 +0200

    Update deploy.sh to get task revision

--- deploy.sh
+++ deploy.sh
@@ -21,7 +21,7 @@ deploy_image() {
 
 update_service() {
     echo "Creating new task/revision"
-    $task = $(aws ecs register-task-definition --cli-input-json file://task.json | $JQ '.taskDefinition.taskDefinitionArn')
+    $task = $(aws ecs register-task-definition --cli-input-json file://task.json | $JQ '.service.taskDefinition')
 
     echo "Updating Service"
     aws ecs update-service --cluster $AWS_CLUSTER --service $AWS_SERVICE --task-definition $task
