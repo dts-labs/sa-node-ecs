8d06649e5a80cb7d72938a8c80c6c4812001baef
David <david92rl@gmail.com>
2016-05-02 13:36:09 +0200

    Fix restart task ecs

--- deploy.sh
+++ deploy.sh
@@ -22,7 +22,16 @@ deploy_image() {
 update_service() {
     echo "Creating new task/revision"
     task=$(aws ecs register-task-definition --cli-input-json file://task.json | $JQ '.taskDefinition.taskDefinitionArn')
-    echo "Task: $task"
+    echo "New Task: $task"
+
+    # Fix: update-service doesn't stop current task
+    revision=$(echo $task | cut -d: -f7)
+    prev_revision=$(($revision-1))
+
+    prev_task="$(echo $task | cut -d: -f1-6):$prev_revision"
+    
+    echo "Stopping current task"
+    aws ecs stop-task --task $prev_task
 
     echo "Updating Service"
     aws ecs update-service --cluster $AWS_CLUSTER --service $AWS_SERVICE --task-definition $task
