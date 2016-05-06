944b2aa5ee816091d13837bdc47f4fe3309271b0
David <david92rl@gmail.com>
2016-05-06 09:47:16 +0000

    SA Changes

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ set -x
 set -o pipefail
 
 JQ="jq --raw-output --exit-status"
+tag="$(date "+%Y-%m-%d_%H%M%S")"
 
 deploy_image() {
     echo "#### Deploying image"
@@ -14,7 +15,6 @@ deploy_image() {
     echo "# Building"
     docker build -t awesome .
     echo "# Tagging"
-    tag="$(date "+%Y-%m-%d_%H%M%S")"
     docker tag awesome 567141585396.dkr.ecr.eu-west-1.amazonaws.com/awesome:$tag
     echo "# Pushing"
     docker push 567141585396.dkr.ecr.eu-west-1.amazonaws.com/awesome:$tag
@@ -22,8 +22,11 @@ deploy_image() {
 }
 
 update_service() {
+    echo "Updating Task definition"
+    sed s/_TAG_/$tag/g task.json > task2.json
+
     echo "Creating new task/revision"
-    task=$(aws ecs register-task-definition --cli-input-json file://task.json | $JQ '.taskDefinition.taskDefinitionArn')
+    task=$(aws ecs register-task-definition --cli-input-json file://task2.json | $JQ '.taskDefinition.taskDefinitionArn')
     echo "New Task: $task"
 
     echo "Updating Service"
