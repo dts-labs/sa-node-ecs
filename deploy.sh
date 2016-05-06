67c908137e486499e4a42df84beae817b963353d
David <david92rl@gmail.com>
2016-05-06 09:24:59 +0000

    SA Changes

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 
 set -e
 set -u
+set -x
 set -o pipefail
 
 JQ="jq --raw-output --exit-status"
@@ -13,9 +14,10 @@ deploy_image() {
     echo "# Building"
     docker build -t awesome .
     echo "# Tagging"
-    docker tag awesome:latest 567141585396.dkr.ecr.eu-west-1.amazonaws.com/awesome:latest
+    tag="$(date "+%Y-%m-%d_%H%M%S")"
+    docker tag awesome:tag 567141585396.dkr.ecr.eu-west-1.amazonaws.com/awesome:tag
     echo "# Pushing"
-    docker push 567141585396.dkr.ecr.eu-west-1.amazonaws.com/awesome:latest
+    docker push 567141585396.dkr.ecr.eu-west-1.amazonaws.com/awesome:tag
     echo "Image deployed!"
 }
 
@@ -23,12 +25,6 @@ update_service() {
     echo "Creating new task/revision"
     task=$(aws ecs register-task-definition --cli-input-json file://task.json | $JQ '.taskDefinition.taskDefinitionArn')
     echo "New Task: $task"
-
-    # Fix: update-service doesn't stop current task
-    prev_task="$(aws ecs list-tasks --cluster $AWS_CLUSTER | $JQ ".taskArns[0]")"
-    
-    echo "Stopping current task: $prev_task"
-    aws ecs stop-task --cluster $AWS_CLUSTER --task $prev_task
 
     echo "Updating Service"
     aws ecs update-service --cluster $AWS_CLUSTER --service $AWS_SERVICE --task-definition $task
